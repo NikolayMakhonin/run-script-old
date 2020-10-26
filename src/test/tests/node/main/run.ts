71119733a99bb293858cd39383b7ad61955adb1c
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-10-26 17:24:40 +0200

    Completed

--- src/test/tests/node/main/run.ts
+++ src/test/tests/node/main/run.ts
@@ -3,6 +3,8 @@ import {run} from '../../../../main/node/helpers'
 import {Writable} from 'stream'
 
 describe('node > run', function () {
+	this.timeout(20000)
+
 	it('require', async function () {
 		process.argv = ['node', './index.js', '--config', '.run-script-rc.js', 'console.log(123)']
 		await require('../../../../main/node/run')
